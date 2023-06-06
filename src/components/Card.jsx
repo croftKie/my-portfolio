21e8397b5b03a6b9dca170055915d2c8a5332b50
Kieran Croft <kiecroft@icloud.com>
2023-06-06 15:24:36 +0100

    added splt and source code button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import code from '../assets/codeFav.png';
+import right from '../assets/right-arrow.png';
 
 class Card extends Component {
     state={
-        showSource : false
+        animSource : false
     }
     render() { 
         const showSourceCodeLink = ()=>{
-            this.setState({showSource : !this.state.showSource})
+            this.setState({animSource : !this.state.animSource})
         }
-        console.log(this.props.img);
 
         return (
-            <div onMouseEnter={showSourceCodeLink} onMouseLeave={showSourceCodeLink} className="card">
-                <a href={this.props.live_link} target='_blank'><img src={this.props.img} alt="" /></a>
-            </div>
+            <>
+                <div onMouseEnter={showSourceCodeLink} onMouseLeave={showSourceCodeLink} className="card">
+                    <a href={this.props.live_link} target='_blank'><img src={this.props.img} alt="" /></a>
+                    <div className="sourceCodeModule">
+                        <img src={code} alt="" />
+                        <a href={this.props.source_link} target='_blank'><h4>source code</h4></a>
+                        <img className= {this.state.animSource ? 'rightImg rightImgAnimated' : 'rightImg'} src={right} alt="" />
+                    </div>
+                </div>
+
+            </>
+
         );
     }
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import Card from './Card';
 import surfer from '../assets/surfer.png';
 import clapper from '../assets/clapperboard.png';
-import cardGraphic from '../assets/proj.png';
 import cliTool from '../assets/cli-tool.png';
+import splt from '../assets/splt.png';
 
 class Content extends Component {
     state = {
@@ -11,13 +11,14 @@ class Content extends Component {
             {img : surfer, live_link : 'https://croftkie.github.io/surf-report-live/', source_link : 'https://github.com/croftKie/surf-report-live'},
             {img : clapper, live_link : 'https://croftkie.github.io/clapperboard/', source_link : 'https://github.com/croftKie/movie-buddy'},
             {img : cliTool, live_link : 'https://www.npmjs.com/package/quick-react-app-builder/', source_link : 'https://www.npmjs.com/package/quick-react-app-builder?activeTab=code'},
+            {img : splt, live_link : 'https://croftkie.github.io/tip-calculator-FM/', source_link : 'https://github.com/croftKie/tip-calculator-FM'},
         ]
     }
 
     render() { 
         return (
             <div className="content">
-                {this.state.projects.map((item, index)=>{
+                {this.state.projects.map((item)=>{
                     return <Card source_link={item.source_link} live_link={item.live_link} img={item.img}/>
                 })}
             </div>
