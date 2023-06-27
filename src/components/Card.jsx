8ee7c92b174388803f1c774a45c3ff9126768847
Kieran Croft <kiecroft@icloud.com>
2023-06-27 13:32:43 +0100

    animations added and info box added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,82 @@
-import React, { Component } from 'react';
-import code from '../assets/codeFav.png';
-import right from '../assets/right-arrow.png';
+import React, { Component, createRef } from "react";
+import code from "../assets/codeFav.png";
+import right from "../assets/right-arrow.png";
+import info from "../assets/info-button.png";
+import { gsap } from "gsap";
 
 class Card extends Component {
-    state={
-        animSource : false
-    }
-    render() { 
-        const showSourceCodeLink = ()=>{
-            this.setState({animSource : !this.state.animSource})
-        }
+  state = {
+    animSource: false,
+    infoBox: false,
+  };
 
-        return (
-            <>
-                <div onMouseEnter={showSourceCodeLink} onMouseLeave={showSourceCodeLink} className="card">
-                    <a href={this.props.live_link} target='_blank'><img src={this.props.img} alt="" /></a>
-                    <div className="sourceCodeModule">
-                        <img src={code} alt="" />
-                        <a href={this.props.source_link} target='_blank'><h4>source code</h4></a>
-                        <img className= {this.state.animSource ? 'rightImg rightImgAnimated' : 'rightImg'} src={right} alt="" />
-                    </div>
-                </div>
+  infoRef = createRef();
+  cardRef = createRef();
+  render() {
+    const showSourceCodeLink = () => {
+      this.setState({ animSource: !this.state.animSource });
+    };
 
-            </>
-
-        );
-    }
+    const showInfo = () => {
+      if (this.state.infoBox) {
+        const tl = gsap.timeline();
+        tl.to(this.cardRef.current, { height: 200 });
+        tl.to(this.infoRef.current, { y: 210, opacity: 0 });
+        setTimeout(() => {
+          this.infoRef.current.classList.add("hide");
+        }, 500);
+        this.setState({ infoBox: !this.state.infoBox });
+      } else {
+        this.infoRef.current.classList.remove("hide");
+        const tl = gsap.timeline();
+        tl.to(this.cardRef.current, { height: 410 });
+        tl.to(this.infoRef.current, { y: 1, opacity: 1, delay: 0 });
+        this.setState({ infoBox: !this.state.infoBox });
+      }
+    };
+    return (
+      <>
+        <div ref={this.cardRef} className="card">
+          <a href={this.props.live_link} target="_blank">
+            <img src={this.props.img} alt="" />
+          </a>
+          <div className="card-nav">
+            <div
+              onMouseEnter={showSourceCodeLink}
+              onMouseLeave={showSourceCodeLink}
+              className="sourceCodeModule"
+            >
+              <img src={code} alt="" />
+              <a href={this.props.source_link} target="_blank">
+                <h4>source code</h4>
+              </a>
+              <img
+                className={
+                  this.state.animSource
+                    ? "rightImg rightImgAnimated"
+                    : "rightImg"
+                }
+                src={right}
+                alt=""
+              />
+            </div>
+            <div className="info">
+              <img onClick={showInfo} src={info} alt="" />
+            </div>
+          </div>
+          <div ref={this.infoRef} className="info-card">
+            <h3>{this.props.app_name}</h3>
+            <p>{this.props.app_description}</p>
+            <div className="tech-images">
+              {this.props.tech_used.map((i) => {
+                return <img src={i} alt="" />;
+              })}
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
- 
+
 export default Card;
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,29 +1,79 @@
-import React, { Component } from 'react';
-import Card from './Card';
-import surfer from '../assets/surfer.png';
-import clapper from '../assets/clapperboard.png';
-import cliTool from '../assets/cli-tool.png';
-import splt from '../assets/splt.png';
+import React, { Component } from "react";
+import Card from "./Card";
+import surfer from "../assets/surfer.png";
+import clapper from "../assets/clapperboard.png";
+import cliTool from "../assets/cli-tool.png";
+import splt from "../assets/splt.png";
+import js from "../assets/js.png";
+import ts from "../assets/ts.png";
+import reactimg from "../assets/react.png";
+import git from "../assets/git.png";
+import reduximg from "../assets/redux.png";
+import sass from "../assets/sass.png";
+import nodejs from "../assets/node.png";
+import python from "../assets/python.png";
 
 class Content extends Component {
-    state = {
-        projects : [
-            {img : surfer, live_link : 'https://croftkie.github.io/surf-report-live/', source_link : 'https://github.com/croftKie/surf-report-live'},
-            {img : clapper, live_link : 'https://croftkie.github.io/clapperboard/', source_link : 'https://github.com/croftKie/movie-buddy'},
-            {img : cliTool, live_link : 'https://www.npmjs.com/package/quick-react-app-builder/', source_link : 'https://www.npmjs.com/package/quick-react-app-builder?activeTab=code'},
-            {img : splt, live_link : 'https://croftkie.github.io/tip-calculator-FM/', source_link : 'https://github.com/croftKie/tip-calculator-FM'},
-        ]
-    }
+  state = {
+    projects: [
+      {
+        img: surfer,
+        live_link: "https://croftkie.github.io/surf-report-live/",
+        source_link: "https://github.com/croftKie/surf-report-live",
+        app_name: "Surfer | Wave Reports",
+        app_description:
+          "Provides up-to-date wave height reports for any surf break in the world. JavaScript front-end with NodeJS proxy server providing API security. ",
+        tech_used: [js, sass, nodejs],
+      },
+      {
+        img: clapper,
+        live_link: "https://croftkie.github.io/clapperboard/",
+        source_link: "https://github.com/croftKie/movie-buddy",
+        app_name: "Clapperboard | Movie Search",
+        app_description:
+          "Algorithm-free movie search database created to provide solve a use case of finding new movies unrelated to previously watched content.",
+        tech_used: [js, sass, reactimg, reduximg],
+      },
+      {
+        img: cliTool,
+        live_link: "https://www.npmjs.com/package/quick-react-app-builder/",
+        source_link:
+          "https://www.npmjs.com/package/quick-react-app-builder?activeTab=code",
+        app_name: "React App Builder | CLI Tool",
+        app_description:
+          "CLI Tool for quickly spinning up a React app with Github and Redux Toolkit functionality - currently at 700 downloads on NPM",
+        tech_used: [js, nodejs, git],
+      },
+      {
+        img: splt,
+        live_link: "https://croftkie.github.io/tip-calculator-FM/",
+        source_link: "https://github.com/croftKie/tip-calculator-FM",
+        app_name: "SPLT | Tip Calculator",
+        app_description:
+          "Tip calculator module created in vanilla JavaScript providing ability to easily split bills at group meals.",
+        tech_used: [js, sass, git],
+      },
+    ],
+  };
 
-    render() { 
-        return (
-            <div className="content">
-                {this.state.projects.map((item)=>{
-                    return <Card source_link={item.source_link} live_link={item.live_link} img={item.img}/>
-                })}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="content">
+        {this.state.projects.map((item) => {
+          return (
+            <Card
+              source_link={item.source_link}
+              live_link={item.live_link}
+              img={item.img}
+              app_name={item.app_name}
+              app_description={item.app_description}
+              tech_used={item.tech_used}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
- 
+
 export default Content;
