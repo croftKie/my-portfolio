9879b0d0ceef66d5bae521272c4d80e0d06172eb
Kieran Croft <croftkie@proton.me>
2023-06-30 17:00:08 +0100

    adjusted animations

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,8 @@ import code from "../assets/codeFav.png";
 import right from "../assets/right-arrow.png";
 import info from "../assets/info-button.png";
 import { gsap } from "gsap";
+import { CustomEase } from "gsap/CustomEase";
+gsap.registerPlugin(CustomEase);
 
 class Card extends Component {
   state = {
@@ -13,6 +15,8 @@ class Card extends Component {
   infoRef = createRef();
   cardRef = createRef();
   render() {
+    const customEase =
+      "M0,0 C0,0 0.039,-0.007 0.06,-0.018 0.12,-0.051 0.163,-0.092 0.22,-0.12 0.235,-0.127 0.255,-0.126 0.272,-0.121 0.288,-0.117 0.303,-0.108 0.315,-0.095 0.337,-0.069 0.354,-0.044 0.37,-0.01 0.393,0.038 0.403,0.072 0.42,0.127 0.44,0.197 0.449,0.238 0.465,0.311 0.504,0.502 0.614,0.744 0.742,0.888 0.868,1.03 1,1 1,1 ";
     const showSourceCodeLink = () => {
       this.setState({ animSource: !this.state.animSource });
     };
@@ -20,8 +24,16 @@ class Card extends Component {
     const showInfo = () => {
       if (this.state.infoBox) {
         const tl = gsap.timeline();
-        tl.to(this.cardRef.current, { height: 200 });
-        tl.to(this.infoRef.current, { y: 210, opacity: 0 });
+        tl.to(this.cardRef.current, {
+          height: 200,
+          ease: CustomEase.create("custom", customEase),
+          duration: 1,
+        });
+        tl.to(this.infoRef.current, {
+          y: 210,
+          opacity: 0,
+          ease: CustomEase.create("custom", customEase),
+        });
         setTimeout(() => {
           this.infoRef.current.classList.add("hide");
         }, 500);
@@ -29,8 +41,18 @@ class Card extends Component {
       } else {
         this.infoRef.current.classList.remove("hide");
         const tl = gsap.timeline();
-        tl.to(this.cardRef.current, { height: 410 });
-        tl.to(this.infoRef.current, { y: 1, opacity: 1, delay: 0 });
+        tl.to(this.cardRef.current, {
+          height: 410,
+          ease: "expo.inOut",
+          duration: 1,
+        });
+        tl.to(this.infoRef.current, {
+          y: 1,
+          opacity: 1,
+          delay: 0,
+          ease: "expo.inOut",
+          duration: 1,
+        });
         this.setState({ infoBox: !this.state.infoBox });
       }
     };
