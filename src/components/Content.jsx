8fded9552e8041f227152548e3a3c87cee2abb1d
Kieran Croft <croftkie@proton.me>
2023-08-01 15:49:48 +0100

    setup logic for different views

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -12,7 +12,6 @@ import git from "../assets/git.png";
 import reduximg from "../assets/redux.png";
 import sass from "../assets/sass.png";
 import nodejs from "../assets/node.png";
-import python from "../assets/python.png";
 import be_a_coder from "../assets/be-a-coder-img.png";
 
 class Content extends Component {
@@ -26,6 +25,7 @@ class Content extends Component {
         app_description:
           "Provides up-to-date wave height reports for any surf break in the world. JavaScript front-end with NodeJS proxy server providing API security. ",
         tech_used: [js, sass, nodejs],
+        type: "project",
       },
       {
         img: clapper,
@@ -35,6 +35,7 @@ class Content extends Component {
         app_description:
           "Algorithm-free movie search database created to provide solve a use case of finding new movies unrelated to previously watched content.",
         tech_used: [js, sass, reactimg, reduximg],
+        type: "project",
       },
       {
         img: cliTool,
@@ -43,8 +44,9 @@ class Content extends Component {
           "https://www.npmjs.com/package/quick-react-app-builder?activeTab=code",
         app_name: "React App Builder | CLI Tool",
         app_description:
-          "CLI Tool for quickly spinning up a React app with Github and Redux Toolkit functionality - currently at 700 downloads on NPM",
+          "CLI Tool for quickly spinning up a React app with Github and Redux Toolkit functionality - over 1000 downloads on NPM",
         tech_used: [js, nodejs, git],
+        type: "package",
       },
       {
         img: splt,
@@ -54,6 +56,7 @@ class Content extends Component {
         app_description:
           "Tip calculator module created in vanilla JavaScript providing ability to easily split bills at group meals.",
         tech_used: [js, sass, git],
+        type: "project",
       },
       {
         img: overload,
@@ -63,6 +66,7 @@ class Content extends Component {
         app_description:
           "A little coder therapy for squashing bugs! Built using KaboomJS for game development and Bootstrap framework for website UI",
         tech_used: [js, sass, git],
+        type: "project",
       },
       {
         img: be_a_coder,
@@ -72,27 +76,73 @@ class Content extends Component {
         app_description:
           "A set of physics simulations to illustrate the life of a programmer building a piece of software. Uses MatterJS for 2d physics simulations.",
         tech_used: [js, git],
+        type: "project",
       },
     ],
+    mode: 0,
   };
 
   render() {
-    return (
-      <div className="content">
-        {this.state.projects.map((item) => {
-          return (
-            <Card
-              source_link={item.source_link}
-              live_link={item.live_link}
-              img={item.img}
-              app_name={item.app_name}
-              app_description={item.app_description}
-              tech_used={item.tech_used}
-            />
-          );
-        })}
-      </div>
-    );
+    if (this.state.mode === 0) {
+      return (
+        <div className="content">
+          {this.state.projects.map((item) => {
+            return (
+              <Card
+                source_link={item.source_link}
+                live_link={item.live_link}
+                img={item.img}
+                app_name={item.app_name}
+                app_description={item.app_description}
+                tech_used={item.tech_used}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
+    if (this.state.mode === 1) {
+      return (
+        <div className="content">
+          {this.state.projects.map((item) => {
+            if (item.type === "project") {
+              return (
+                <Card
+                  source_link={item.source_link}
+                  live_link={item.live_link}
+                  img={item.img}
+                  app_name={item.app_name}
+                  app_description={item.app_description}
+                  tech_used={item.tech_used}
+                />
+              );
+            }
+          })}
+        </div>
+      );
+    }
+
+    if (this.state.mode === 2) {
+      return (
+        <div className="content">
+          {this.state.projects.map((item) => {
+            if (item.type === "package") {
+              return (
+                <Card
+                  source_link={item.source_link}
+                  live_link={item.live_link}
+                  img={item.img}
+                  app_name={item.app_name}
+                  app_description={item.app_description}
+                  tech_used={item.tech_used}
+                />
+              );
+            }
+          })}
+        </div>
+      );
+    }
   }
 }
 
