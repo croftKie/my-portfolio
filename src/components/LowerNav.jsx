d0d29145b1f505de03ac32bbe45ce4339370d30a
Kieran Croft <croftkie@proton.me>
2023-08-16 14:30:07 +0100

    nav rebuild - mobile and desktop

--- src/components/LowerNav.jsx
+++ src/components/LowerNav.jsx
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import About from "./About";
 
 class LowerNav extends Component {
-    state = {
-        moduleOpen : false
-    }
-    buttonRef = React.createRef(null)
+  state = {
+    moduleOpen: false,
+  };
+  buttonRef = React.createRef(null);
 
-    changeStyle = (e)=>{
-        Array.from(this.buttonRef.current.children).forEach((button)=>{
-            button.classList.remove("active");
-        });
-        this.buttonRef.current.children[e].classList.add("active");
-    }
+  changeStyle = (e) => {
+    Array.from(this.buttonRef.current.children).forEach((button) => {
+      button.classList.remove("active");
+    });
+    this.buttonRef.current.children[e].classList.add("active");
+  };
 
-
-    render() { 
-        return ( 
-            <div ref={this.buttonRef} className="lower-nav">
-                <button className='active' onClick={(e)=>{this.changeStyle(0), this.props.onModeChange("all")}}>All Builds</button>
-                <button onClick={(e)=>{this.changeStyle(1), this.props.onModeChange("projects")}}>Projects Only</button>
-                <button onClick={(e)=>{this.changeStyle(2), this.props.onModeChange("packages")}}>Published Packages</button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="lower-nav">
+        <About />
+        <div ref={this.buttonRef} className="buttons">
+          <button
+            className="active"
+            onClick={(e) => {
+              this.changeStyle(0), this.props.onModeChange("all");
+            }}
+          >
+            All Builds
+          </button>
+          <button
+            onClick={(e) => {
+              this.changeStyle(1), this.props.onModeChange("projects");
+            }}
+          >
+            Projects Only
+          </button>
+          <button
+            onClick={(e) => {
+              this.changeStyle(2), this.props.onModeChange("packages");
+            }}
+          >
+            Published Packages
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default LowerNav;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import About from './About';
-import SocialBar from './socialBar';
+import React, { Component } from "react";
+import About from "./About";
+import SocialBar from "./socialBar";
 
 class Nav extends Component {
-    state = {
-        moduleOpen : false
-    }
-    render() { 
-        return ( 
-            <div className="nav">
-                <div className="info">
-                    <div className="name">
-                        <h3>Kieran Croft</h3>
-                        <p>Fullstack Software Developer</p>
-                    </div>
-                    <SocialBar showToastMessage={this.props.showToastMessage}/>
-                </div>
-                <About showToastMessage={this.props.showToastMessage}/>
-            </div>
-        );
-    }
+  state = {
+    moduleOpen: false,
+  };
+  render() {
+    return (
+      <div className="nav">
+        <div className="info">
+          <div className="name">
+            <h3>Kieran Croft</h3>
+            <p>Fullstack Software Developer</p>
+          </div>
+          <SocialBar showToastMessage={this.props.showToastMessage} />
+        </div>
+        <div className="about open">
+          <p>I build things for the web.</p>
+          <p>
+            Experienced in front-end and back-end web development, I create
+            robust applications using a range of technologies.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Nav;
